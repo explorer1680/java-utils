3caa2ae65742918de7ec9276322293605aff9d7a
echeng <echeng@192.168.2.25>
2021-02-28 11:44:03 -0500

    update doc.

---
+++ src/test/java/personal/unittest/junit5/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package personal.unittest.junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	void firstTest() {
+		System.out.println("First test");
+	}
+	
+
+}
